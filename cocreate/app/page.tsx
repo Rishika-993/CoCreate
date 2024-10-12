2a8666f7f96030b032d5aa4567bf35009436ae04
Rishika-993 <rishikaagrawal22@gmail.com>
2024-10-12 15:24:59 +0530

    deploy correction

--- cocreate/app/page.tsx
+++ cocreate/app/page.tsx
@@ -1,12 +1,8 @@
-import Image from "next/image";
 import HomePage from "./components/Homepage/HomePage";
-import { Sidebar } from "lucide-react";
-import RightSidebar from "./components/Homepage/RightSideBar";
 
 export default function Home() {
   return (
     <>
-    {/* <Sidebar /> */}
     <HomePage />
     </>
   );
